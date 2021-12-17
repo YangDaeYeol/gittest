2678d2af102cb3c66165b6ef2b18b684bdefc1ff
daeye <daeye@192.168.219.104>
2021-12-17 10:16:19 +0900

    model.vo에 member클래스 작성

--- gitTest/src/git/com/main/Run.java
+++ gitTest/src/git/com/main/Run.java
@@ -5,7 +5,7 @@ public class Run {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("나의 첫 git실행!");
-		
+
 		int age = 19;
 		String address = "경기도 시흥시";
 		
